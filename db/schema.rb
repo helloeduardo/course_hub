d6e19d5e57910f963823d59b44e9f8454ad2e5e4
Eduardo Parra <helloeduardo@icloud.com>
2021-03-01 00:30:03 -0700

    Add registrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_064517) do
+ActiveRecord::Schema.define(version: 2021_03_01_070453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_03_01_064517) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "registrations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_registrations_on_course_id"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -29,4 +38,6 @@ ActiveRecord::Schema.define(version: 2021_03_01_064517) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "registrations", "courses"
+  add_foreign_key "registrations", "users"
 end
